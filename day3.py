fea57416918c0ac55639eb41e8daca19500b39f4
fuyixuannnn <89377911+fuyixuannnn@users.noreply.github.com>
2021-08-30 10:21:30 +0800

    Update day3.py

--- day3.py
+++ day3.py
@@ -16,11 +16,25 @@
                 输入错误
             输入q或Q，退出并结算，打印小条
 任务：
-    1.尽量多的添加商品
-    2.10张xxx优惠券（x折），20张xxx优惠券（x折）
+    1.打印小票，合并同类计数
+    2.10张iPhone优惠券（8折），20张HUAWEI优惠券（9折）
         进入商城可随机抽取一张优惠券，在最后结算时使用优惠券对相应商品打折
 '''
-# 1.商品
+# 抽奖
+import random
+print("欢迎欢迎~ 恭喜获得抽奖机会！您可抽取一张特定优惠券")
+input("点击回车就可以抽奖了哦~")
+num = random.randint(1,30)
+if 0 < num < 11:
+    print("恭喜你!获得iPhone8折优惠券！")
+    coupon = 3
+    discount = 0.8
+if 10 < num < 31:
+    print("恭喜你！获得HUAWEI9折优惠券")
+    coupon = 1
+    discount = 0.9
+
+# 商品
 shop = [
     ["机械革命",15000],
     ["HUAWEI watch",1200],
@@ -30,7 +44,7 @@ shop = [
     ["老干妈",13]
 ]
 
-# 2.初始化您的余额
+# 初始化您的余额
 money = input("请输入您的钱包余额:")
 money = int(money)
 
@@ -56,11 +70,17 @@ while True:
             # 看钱是否足够
             if money > shop[chose][1]:
                 mycart.append(shop[chose])
-                # 钱减去
-                money = money - shop[chose][1]
-                print("恭喜，成功添加购物车，您的余额还剩：￥",money)
+                if chose == coupon:
+                    discount_money = shop[chose][1] * discount
+                    coupon = 100
+                    money = money - discount_money
+                    print("恭喜，成功添加购物车，优惠券已使用。您的余额还剩：￥",money)
+                else:
+                    money = money - shop[chose][1]
+                    print("恭喜，成功添加购物车，您的余额还剩：￥",money)
             else:
-                print("对不起，穷鬼，余额不足，请到商城去购买！")
+                if money < shop[chose][1]:
+                    print("对不起，穷鬼，余额不足，请到别的商城去购买！")
     elif chose == 'q' or chose == 'Q':
         print("欢迎下次光临！")
         break
@@ -80,6 +100,4 @@ for i in dic:
     print(i,dic[i])
 print("您的钱包余额还剩: ",money)
 print("-----------------------------------------")
-
-
 
